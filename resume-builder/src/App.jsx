fee5376c5a596e1d8f4f7b47c0759d10ba1b5bfb
NMGVox <nijeemgreene@gmail.com>
2023-08-12 14:44:49 -0400

    Set-up react environment

---
+++ resume-builder/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="input-container">
+        <div className="category">
+          Personal Info
+        </div>
+      </div>
+      <div className="resume-display">
+        <div className="resume">
+        </div>
+      </div>
+    </>
+  );
+}
+export default App
